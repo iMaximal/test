f2ccbb2b9e9ad0b43ec7d2e0784eaea20b582760
iMaximal <iMaximal@users.noreply.github.com>
2017-07-03 17:31:26 +0500

    Merge branch 'origin/master' Test Less
    # Conflicts:
    #	src/index.js

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 var $ = require('jquery');
-var str = require('./constant');
+// var str = require('./constant');
 require('./less');
 
 var App = function () {
-  console.log(str);
-  $('body').html('<div class="b-test"><h3 class="b-test__heading">Heading</h3></div>');
+  // console.log(str);
+  $('body').html('<div class="b-test"><h3 class="b-test__heading">Heading</h3><div> </div></div>');
 };
 
 var app = new App();
